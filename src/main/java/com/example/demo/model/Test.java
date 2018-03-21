b9535386042af2e3b5db5634f54b2d7ce98bfaad
Adrian <adrian.kozlowski[@]outlook.com>
2018-03-21 08:53:20 +0100

    test

--- src/main/java/com/example/demo/model/Test.java
+++ src/main/java/com/example/demo/model/Test.java
@@ -9,4 +9,22 @@ public class Test {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
