1f2ce383bd97f80a17404dfc78dccef19cfe16de
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-03-23 19:17:27 +0100

    upload pliku

--- src/main/java/com/example/demo/controllers/MyController.java
+++ src/main/java/com/example/demo/controllers/MyController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controllers;
 
 import com.example.demo.classes.SessionClass;
+import com.example.demo.model.Test;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MyController {
@@ -28,5 +30,12 @@ public class MyController {
     public String gett() {
         sessionClass.setCounter(sessionClass.getCounter() + 1);
         return "index";
+    }
+
+    @PostMapping(value = "/add", produces = "application/json")
+    @ResponseBody
+    public Test add(@ModelAttribute Test test, @RequestParam("plik") MultipartFile file) {
+
+        return test;
     }
 }
--- src/main/java/com/example/demo/controllers/MyOtherController.java
+++ src/main/java/com/example/demo/controllers/MyOtherController.java
@@ -4,6 +4,7 @@ import com.example.demo.classes.SessionClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 public class MyOtherController {
@@ -14,9 +15,10 @@ public class MyOtherController {
 
 
     @GetMapping("/index2")
-    public String getList() {
+    public String getList(SessionStatus sessionStatus) {
         sessionClass.setCounter(sessionClass.getCounter() + 1);
-        System.out.println(sessionClass.getCounter());
+
+
         return "index";
     }
 
