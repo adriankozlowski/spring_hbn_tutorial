aa3fcf35d84aa12a4adc72ed2159375d7660c832
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-03-25 09:42:46 +0200

    formularz surowy

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.Person;
 import com.example.demo.services.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,5 +20,12 @@ public class HelloController {
     public String greetings(ModelMap map) {
         map.addAttribute("greeting", helloService.greetings());
         return "hello";
+    }
+
+    @PostMapping("/hello")
+    @ResponseBody
+    public Person submitForm(@ModelAttribute Person person) {
+
+        return person;
     }
 }
